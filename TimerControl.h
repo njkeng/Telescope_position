85f68d3c16f9400b61d2c598a478ca4dd5318258
njkeng <njkotzur@hotmail.com>
2019-09-03 10:22:25 +1000

    Upload original files

---
+++ TimerControl.h
@@ -0,0 +1,18 @@
+#ifndef _TIMERCONTROL_
+#define _DUECANLAYER_
+
+struct Timer
+{
+  int  nCount;
+  bool bStart;
+  bool bExpired;
+};
+
+#define TIMERFREQUENCY                    1000    // Time = 1 sec / TIMERFREQUENCY
+#define LCD_FREQ                    	    200	  // Frequency to update the LCD display
+
+
+#define TIMERS                            1       // Number of timers
+#define pTimerLCD                         pTimer[0]
+
+#endif
